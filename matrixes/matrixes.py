7e7f9048a6a61dca09424554e3eda5117dec21dc
jazehin <jazehin@outlook.com>
2022-11-21 18:05:05 +0100

    matrixes task done

--- matrixes/matrixes.py
+++ matrixes/matrixes.py
@@ -1,19 +1,35 @@
 # main diagonal -> going from upper left corner to bottom right corner
 
-def isLowerTriangular(matrix):
-    loverTriangular = True
-    length = len(matrix)
-    i = 2
-    j = 1
-    while (i < length):
-        
-    return True
-
-
-
-
 def isUpperTriangular(matrix):
-    return True    
+    upperTriangular = True
+    length = len(matrix)
+    i = 1
+    j = 0
+    while (i < length and upperTriangular):
+        while (j < i and upperTriangular):
+            if (matrix[i][j] != 0):
+                upperTriangular = False
+            j = j + 1
+        i = i + 1
+        j = 0
+    return upperTriangular
+
+
+
+
+def isLowerTriangular(matrix):
+    lowerTriangular = True
+    length = len(matrix)
+    i = 0
+    j = 1
+    while (i < length - 1 and lowerTriangular):
+        j = i + 1
+        while (j < length and lowerTriangular):
+            if (matrix[i][j] != 0):
+                lowerTriangular = False
+            j = j + 1
+        i = i + 1
+    return lowerTriangular 
 
 
 
@@ -44,9 +60,14 @@ while (i < numberOfMatrixes):
 
         matrix.append(array)
         j = j + 1
-
-    loverTriangular = isLowerTriangular(matrix)
-    upperTriangular = isUpperTriangular(matrix)
-    diagonal = isDiagonal(matrix)
+    
+    if (isDiagonal(matrix)):
+        print("Diagonal")
+    elif (isLowerTriangular(matrix)):
+        print("Lower triangular")
+    elif (isUpperTriangular(matrix)):
+        print("Upper triangular")
+    else:
+        print("Neither diagonal nor triangular")
 
     i = i + 1
